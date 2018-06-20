f042e12f4e87f751d1ff8c3ea3ab8beec87afe64
markummitchell <markummitchell@gmail.com>
2018-06-19 20:34:55 -0700

    Doxygen docs

---
+++ src/PatternLayout.hh
@@ -0,0 +1,31 @@
+ #ifndef PATTERN_LAYOUT_HH
+#define PATTERN_LAYOUT_HH
+
+#include "log4cpp/Configurator.hh"
+#include "log4cpp/Layout.hh"
+#include <string>
+
+namespace log4cpp {
+
+  class LoggingEvent;
+
+  /// Noop class that mimics the same class in the log4cpp library
+  class PatternLayout : public Layout
+  {
+  public:
+    /// Single constructor
+    PatternLayout ();
+    virtual ~PatternLayout ();
+
+    /// Noop method for formatting an event
+    virtual std::string format(const LoggingEvent &event);
+
+    /// Noop method for setting the conversion pattern
+    virtual void setConversionPattern (const std::string &conversionPattern) throw (ConfigureFailure);
+
+  private:
+    std::string m_conversionPattern;
+  };
+}
+
+#endif // PATTERN_LAYOUT_HH
